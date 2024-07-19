fb548ab73c401833b7091bb8ff7b9b59694255cf
byaangeel <161104656+byaangeel@users.noreply.github.com>
2024-07-19 13:43:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const sideMenu = document.getElementById('side-menu');
+    if (sideMenu.style.width === '250px') {
+        sideMenu.style.width = '0';
+    } else {
+        sideMenu.style.width = '250px';
+    }
+}
+
